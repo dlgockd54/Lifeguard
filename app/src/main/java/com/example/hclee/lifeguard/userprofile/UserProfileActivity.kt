c7209b62cb29322062c904d6cd35406a6f196b11
hclee <dlgockd45@gmail.com>
2019-04-04 14:29:17 +0900

    Add UserProfileActivity

---
+++ app/src/main/java/com/example/hclee/lifeguard/userprofile/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hclee.lifeguard.userprofile
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UserProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
